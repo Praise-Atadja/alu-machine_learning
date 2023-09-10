212a455ee9a5afc9317311e02983418d3db74f25
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-10 22:46:42 +0100

    Update 14-saddle_up.py

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-np_matmul performs matrix multiplication using NumPy, 
-ensuring input matrices mat1 and mat2 are non-empty ndarrays. 
-No loops or conditionals.
+This module contains the function:
+performs matrix multiplication,
+assume mat1 and mat2 are never empty and
+can be interpreted as numpy.ndarray
 """
 import numpy as np
 
