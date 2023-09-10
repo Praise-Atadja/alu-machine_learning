259aed8a7c947e61e8e33f8eb92594964684d34c
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-10 22:40:46 +0100

    Update 14-saddle_up.py

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 "Performs matrix multiplication using NumPy without loops or conditionals."
 import numpy as np
+
+
 def np_matmul(mat1, mat2):
     result = np.dot(mat1, mat2)
     return result
