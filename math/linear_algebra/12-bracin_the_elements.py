32b24108c6e9d9cf67023b92d2052f65b94cf54d
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-10 22:30:15 +0100

    Update 12-bracin_the_elements.py

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def np_elementwise(mat1, mat2):
+    elementwise_sum = mat1 + mat2
+    elementwise_diff = mat1 - mat2
+    elementwise_product = mat1 * mat2
+    elementwise_quotient = mat1 / mat2
+    
+    return (elementwise_sum, elementwise_diff, elementwise_product, elementwise_quotient)
