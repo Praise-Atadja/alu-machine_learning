e512a54d37b5356ff99401200727edcc97ebbe0a
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-10 21:59:14 +0100

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-matrix1 = [[1, 2, 3], [4, 5, 6]]
-matrix2 = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
 
-shape1 = matrix_shape(matrix1)
-shape2 = matrix_shape(matrix2)
+matrix_shape = __import__('2-size_me_please').matrix_shape
 
-print(shape1)  # Output will be [2, 3]
-print(shape2)  # Output will be [2, 2, 3]
+mat1 = [[1, 2], [3, 4]]
+print(matrix_shape(mat1))
+mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+print(matrix_shape(mat2))
