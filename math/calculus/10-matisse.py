7b285ac7887c96e5638b9bed54e2b82f3b6dd754
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-24 23:03:03 +0200

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+""" The derivative of polynomial
+"""
+
 
 def poly_derivative(poly):
-    # Check if poly is a valid list of coefficients
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly):
-        return None
-    
-    # Calculate the derivative of the polynomial
-    derivative = []
-    for i in range(1, len(poly)):
-        # Multiply each coefficient by its corresponding power of x
-        derivative.append(poly[i] * i)
-    
-    # If the derivative is all zeros, return [0]
-    if all(coeff == 0 for coeff in derivative):
-        return [0]
-    
-    return derivative
+    """ Find the derivative of polynomial
+    Example: if f(x) = x^3 +4x^2 + 9x +5, poly is equal to [5, 3, 4, 1]
 
+    Args:
+        poly (_type_): _description_
+    """
+    derivative = []
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    for i in range(len(poly)-1, 0, -1):
+
+        derivative.append(poly[i]*i)
+    return derivative[::-1]
