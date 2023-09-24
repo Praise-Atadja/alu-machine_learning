61f594824115b3606b10cb241b307421a58cdd18
Praise-Atadja <p.atadja@alustudent.com>
2023-09-24 03:42:35 +0100

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -9,24 +9,14 @@ def poly_integral(poly, C=0):
     if not isinstance(C, int):
         return None
     
-    # Initialize the result list with C as the constant term
-    result = [C]
-    
-    # Iterate through the coefficients in poly and integrate each term
+    # Calculate the integral of the polynomial
+    integral = [C]  # Initialize the result list with the integration constant C
     for i, coeff in enumerate(poly):
         if isinstance(coeff, int):
             # If the coefficient is an integer, perform integer division
-            result.append(coeff // (i + 1))
+            integral.append(coeff // (i + 1))
         else:
             # If the coefficient is a float, perform regular division
-            result.append(coeff / (i + 1))
+            integral.append(coeff / (i + 1))
     
-    # Remove any trailing zeros
-    while result and result[-1] == 0:
-        result.pop()
-    
-    # Add 0 as the constant term if the result is empty
-    if not result:
-        result.append(0)
-    
-    return result
+    return integral
