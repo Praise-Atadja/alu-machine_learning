3eb3bc03c81a8634307702d91239794700002524
Praise-Atadja <p.atadja@alustudent.com>
2023-09-24 03:13:55 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,20 +7,21 @@
         n (int): The stopping condition for the sum.
 
     Returns:
-        int or None: The integer value of the sum of squares from 1 to n. 
-                     Returns None if n is not a valid number.
+        int or None: The integer value of the sum of squares from 1 to n.
+        Returns None if n is not a valid number.
 """
 
 
 def summation_i_squared(n):
     # Check if n is a valid number
+    # Calculate the sum of squares using the formula
+    # This formula efficiently computes the sum of squares of integers
     if not isinstance(n, int) or n < 1:
         return None
 
-    # Calculate the sum of squares using the formula
-    # This formula efficiently computes the sum of squares of integers
-    # without using loops or recursion.
+
     result = (n * (n + 1) * (2 * n + 1)) // 6
+    # without using loops or recursion.
 
     # Return the result, which is the sum of squares
     return result
