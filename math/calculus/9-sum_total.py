fa7527d05b438fe62c40542406d8633a0e5161c3
Praise-Atadja <p.atadja@alustudent.com>
2023-09-24 02:56:58 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Module for calculating the sum of squares of integers."""
+
 def summation_i_squared(n):
     # Check if n is a valid number
     if not isinstance(n, int) or n < 1:
@@ -9,6 +11,6 @@ def summation_i_squared(n):
     # This formula efficiently computes the sum of squares of integers
     # without using loops or recursion.
     result = (n * (n + 1) * (2 * n + 1)) // 6
-    
+
     # Return the result, which is the sum of squares
     return result
