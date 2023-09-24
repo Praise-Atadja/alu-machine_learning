e55e40e08fda153557733a255d51272cdddc3137
_Orlyaseye <106454335+Praise-Atadja@users.noreply.github.com>
2023-09-24 23:05:19 +0200

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
+"""
+Calculates the integral of a polynomial
+"""
+
 
 def poly_integral(poly, C=0):
-    # Check if poly is a valid list of coefficients
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly):
+    """ Calculates the integral of a polynomial
+    """
+    integral = []
+    # check for the validity of the inputs
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
-    
-    # Check if C is a valid integer
-    if not isinstance(C, int):
-        return None
-    
-    # Calculate the integral of the polynomial
-    integral = [C]  # Initialize the result list with the integration constant C
-    
-    for i, coeff in enumerate(poly):
-        # Calculate the integral for each coefficient
-        integral_coeff = coeff / (i + 1)
-        integral.append(integral_coeff)
-    
-    return integral
+    # calculate the cofficients
+    for i in range(len(poly)-1, 0, -1):
+        integral.append(poly[i]/(i+1))
+
+    integral.append(poly[0])
+    integral.append(C)
+    # fit the output like on the intranet
+    if len(poly) == 1 and poly[0] == 0:
+        integral = [C]
+    for i in range(len(integral)):
+        if integral[i] % 1 == 0:
+            integral[i] = int(integral[i])
+
+    return integral[::-1]
