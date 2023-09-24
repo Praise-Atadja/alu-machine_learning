79f3ad80fdd6e8560fe58573ddf095a36bd7abd5
Praise-Atadja <p.atadja@alustudent.com>
2023-09-24 02:48:50 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,8 +4,8 @@ def summation_i_squared(n):
     # Check if n is a valid number
     if not isinstance(n, int) or n < 1:
         return None
-    
+
     # Calculate the sum of squares using the formula
     result = (n * (n + 1) * (2 * n + 1)) // 6
-    
+
     return result
