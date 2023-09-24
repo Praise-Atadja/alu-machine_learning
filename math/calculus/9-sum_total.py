af176ee3b0e30868497161b62491c3ba3410e2fe
Praise-Atadja <p.atadja@alustudent.com>
2023-09-24 02:50:08 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,9 @@ def summation_i_squared(n):
     if not isinstance(n, int) or n < 1:
         return None
 
-    # Calculate the sum of squares using the formula
+    # Calculate the sum of squares using the formula: (n * (n + 1) * (2 * n + 1)) / 6
+    # This formula efficiently computes the sum of squares of integers from 1 to n
+    # without using loops or recursion.
     result = (n * (n + 1) * (2 * n + 1)) // 6
 
     return result
