efd952545b455a5cae7a691b9a8e212dc8175dae
Praise-Atadja <p.atadja@alustudent.com>
2023-10-01 21:15:10 +0100

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -13,7 +13,7 @@ class Poisson:
     """
 
     def __init__(self, data=None, lambtha=1.):
-    
+ 
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
