7f7fa815e483c8dfdc2aca708026c4c7bcee2b74
Praise-Atadja <p.atadja@alustudent.com>
2023-10-01 21:29:29 +0100

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -4,16 +4,11 @@
    This module creates a Poisson distribution class.
 
 """
-
-
 class Poisson:
     """
-    Creates a Poisson distribution class.
-
+    Calculates the value of the PMF for a given number of “successes”
     """
-
     def __init__(self, data=None, lambtha=1.):
-
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -24,3 +19,23 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of "successes."
+
+        Args:
+            k (int or float): The number of "successes."
+
+        Returns:
+            float: The PMF value for k.
+        """
+        k = int(k)  # Convert to integer
+        if k < 0:
+            return 0
+        result = 1.0
+        for i in range(1, k + 1):
+            result *= self.lambtha / i
+        result *= 2.71828 ** (-self.lambtha)
+        return result
+    
