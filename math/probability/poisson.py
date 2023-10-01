65c1bf9631712c55d01060022085683656d38e17
Praise-Atadja <p.atadja@alustudent.com>
2023-10-01 20:37:08 +0100

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -11,14 +11,4 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data) / len(data)
-
-# Example usage:
-# Create an instance of Poisson with lambtha specified
-poisson1 = Poisson(lambtha=2.5)
-print(poisson1.lambtha)  # Output: 2.5
-
-# Create an instance of Poisson with data to estimate lambtha
-data_points = [1, 2, 3, 4, 5]
-poisson2 = Poisson(data_points)
-print(poisson2.lambtha)  # Output: 3.0
+            self.lambtha = float(sum(data) / len(data))
