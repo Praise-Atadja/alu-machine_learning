16575347a816e70a49f9aadc43c8a9fb7c792add
Praise-Atadja <p.atadja@alustudent.com>
2023-10-01 21:31:12 +0100

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -36,6 +36,6 @@ class Poisson:
         result = 1.0
         for i in range(1, k + 1):
             result *= self.lambtha / i
-        result *= 2.71828 ** (-self.lambtha)
+        result *= 2.7182818285 ** (-self.lambtha)
         return result
     
