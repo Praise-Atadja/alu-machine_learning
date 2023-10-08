0359fd1ef02015f6bec092bcc26d90895f8bf5a9
Praise-Atadja <p.atadja@alustudent.com>
2023-10-08 19:51:12 +0300

    a numpy.ndarray containing the convolved images

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """This module performs a valid convolution on grayscale images:"""
+
 import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
@@ -10,10 +11,9 @@ def convolve_grayscale_valid(images, kernel):
     output_w = w - kw + 1
     output = np.zeros((m, output_h, output_w))
 
-    for i in range(m):
-        for j in range(output_h):
-            for k in range(output_w):
-                patch = images[i, j:j+kh, k:k+kw]
-                output[i, j, k] = np.sum(patch * kernel)
+    for i in range(output_h):
+        for j in range(output_w):
+            for k in range(m):
+                output[k, i, j] = np.sum(images[k, i:i+kh, j:j+kw] * kernel)
 
     return output
