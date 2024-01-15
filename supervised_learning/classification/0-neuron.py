6c7d18f51fd81e52024c06a887c05f7439c09d59
Praise-Atadja <p.atadja@alustudent.com>
2024-01-15 18:10:20 +0200

    Update 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -3,6 +3,8 @@
 """Module defines a single neuron performing binary classification"""
 
 import numpy as np
+
+
 class Neuron:
     """ Class that defines a single neuron performing binary classification """
 
@@ -12,8 +14,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        
-        
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
