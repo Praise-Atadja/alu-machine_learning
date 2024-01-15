8cf944010bd1eb31e717d1156e0e8b9f7f9eec1c
Praise-Atadja <p.atadja@alustudent.com>
2024-01-15 23:18:36 +0200

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -6,10 +6,10 @@ import numpy as np
 
 
 class Neuron:
-    """Each private attribute should have a corresponding getter function (no setter function)."""
-    
-    
-    def __init__(self, nx):
+    """Each private attribute should have a 
+corresponding getter function (no setter function)."""
+
+    def __init__(self, nx,):
 
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
@@ -19,11 +19,11 @@ class Neuron:
         self.b = 0
         self.A = 0
 
-    def get__b(self):
-        return self.b
+        def get__b(self):
+            return self.__b
 
-    def get__A(self):
-        return self.A
+        def get__A(self):
+            return self.__A
 
-    def get__W(self):
-        return self.W
+        def get__W(self):
+            return self.__W
