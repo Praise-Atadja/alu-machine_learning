83d8275691d13398f0cb75398ab291d0d4647bff
Praise-Atadja <p.atadja@alustudent.com>
2024-01-15 23:55:26 +0200

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -14,18 +14,18 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.__W = np.random.randn(1, 0)
-        self.__b = 0
-        self.__A = 0
+        self._W = np.random.randn(1, 0)
+        self._b = 0
+        self._A = 0
 
     def get_b(self):
         """getter method for b"""
-        return self.__b
+        return self._b
 
     def get_A(self):
         """getter method for A"""
-        return self.__A
+        return self._A
 
     def get_W(self):
         """getter method for W"""
-        return self.__W
+        return self._W
