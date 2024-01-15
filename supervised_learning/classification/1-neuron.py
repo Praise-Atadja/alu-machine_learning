635d5d081175dd473db6ca0e94764d20cd4f10cc
Praise-Atadja <p.atadja@alustudent.com>
2024-01-15 23:39:48 +0200

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -8,13 +8,13 @@ import numpy as np
 class Neuron:
     """Each private attribute should have a corresponding getter function."""
 
-    def __init__(self, nx,):
+    def __init__(self, nx):
 
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.__W = np.random.normal(size=(1, nx))
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
