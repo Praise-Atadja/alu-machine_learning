5ed41b2e47cf9e6bc64d1d8b6452dcb9cd6622e7
Praise-Atadja <p.atadja@alustudent.com>
2024-01-15 18:38:11 +0200

    corresponding getter function

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -17,3 +17,6 @@ class Neuron:
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
+    
+    def get_private_attributes(self):
+        return self.__private_attributes
