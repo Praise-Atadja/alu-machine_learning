7b304bf1cfed2024db8c76a82186480d294e6198
Praise-Atadja <p.atadja@alustudent.com>
2024-01-17 07:08:51 +0200

    Neuron Cost

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""Module defines a single neuron performing binary classification"""
+
+import numpy as np
+
+
+class Neuron:
+    """Each private attribute should have a corresponding getter function."""
+
+    def __init__(self, nx):
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self._W = np.random.randn(1, nx)
+        self._b = 0
+        self._A = 0
+
+    @property
+    def b(self):
+        """getter method for b"""
+        return self._b
+
+    @property
+    def A(self):
+        """getter method for A"""
+        return self._A
+
+    @property
+    def W(self):
+        """getter method for W"""
+        return self._W
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.dot(self._W, X) + self._b
+        self._A = 1 / (1 + np.exp(-z))
+        return self._A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / Y.shape[1]
+        return cost
