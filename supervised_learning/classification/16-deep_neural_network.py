9728e9331a8fe2bdc961700cbb74a25797bf1da1
Praise-Atadja <p.atadja@alustudent.com>
2024-01-19 00:45:17 +0200

    Privatize DeepNeuralNetwork

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -62,3 +62,34 @@ class DeepNeuralNetwork:
     def weights(self):
         """Getter method for weights"""
         return self.__weights
+
+    def __init__(self, nx, layers):
+        """Constructor method for deep neural network"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if (
+            type(layers) is not list
+            or len(layers) < 1
+            or min(layers) < 1
+        ):
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(len(layers)):
+            if i == 0:
+                self.__weights['W1'] = np.random.randn(
+                    layers[0], nx
+                ) * np.sqrt(2 / nx)
+                self.__weights['b1'] = np.zeros([layers[0], 1])
+            else:
+                self.__weights[
+                    'W{}'.format(i + 1)
+                ] = np.random.randn(
+                    layers[i], layers[i - 1]
+                ) * np.sqrt(2 / layers[i - 1])
+                self.__weights['b{}'.format(i + 1)] = np.zeros(
+                    [layers[i], 1]
+                )
