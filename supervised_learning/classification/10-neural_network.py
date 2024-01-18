ec563473bb8985b5a9d7526d2525cfa5f2c7b5b1
Praise-Atadja <p.atadja@alustudent.com>
2024-01-18 13:32:29 +0200

    NeuralNetwork Forward Propagation

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -19,49 +19,49 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        self._W1 = np.random.randn(nodes, nx)
-        self._b1 = np.zeros([nodes, 1], dtype=float)
-        self._A1 = 0
-        self._W2 = np.random.randn(1, nodes)
-        self._b2 = 0
-        self._A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros([nodes, 1], dtype=float)
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
         """getter method for W1"""
-        return self._W1
+        return self.__W1
 
     @property
     def b1(self):
         """getter method for b1"""
-        return self._b1
+        return self.__b1
 
     @property
     def A1(self):
         """getter method for A1"""
-        return self._A1
+        return self.__A1
 
     @property
     def W2(self):
         """getter method for W2"""
-        return self._W2
+        return self.__W2
 
     @property
     def b2(self):
         """getter method for b2"""
-        return self._b2
+        return self.__b2
 
     @property
     def A2(self):
         """getter method for A2"""
-        return self._A2
+        return self.__A2
 
     def forward_prop(self, X):
         """Calculates the forward propagation of the neural network"""
-        Z1 = np.dot(self._W1, X) + self._b1
+        Z1 = np.dot(self.__W1, X) + self.__b1
         sigmoid1 = 1 / (1 + np.exp(-Z1))
-        self._A1 = sigmoid1
-        Z2 = np.dot(self._W2, self._A1) + self._b2
+        self.__A1 = sigmoid1
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
         sigmoid2 = 1 / (1 + np.exp(-Z2))
-        self._A2 = sigmoid2
-        return self._A1, self._A2
+        self.__A2 = sigmoid2
+        return self.__A1, self.__A2
