0b8caae372df508668b39b55d68085b82ff9b3f4
Praise-Atadja <p.atadja@alustudent.com>
2024-01-19 00:16:25 +0200

    Update 12-neural_network.py

--- supervised_learning/classification/12-neural_network.py
+++ supervised_learning/classification/12-neural_network.py
@@ -74,7 +74,7 @@ class NeuralNetwork:
 
     def evaluate(self, X, Y):
         """Evaluates the neural network’s predictions"""
-        A2 = self.forward_prop(X)
+        _, A2 = self.forward_prop(X)
         cost = self.cost(Y, A2)
         prediction = np.where(A2 >= 0.5, 1, 0)
         return prediction, cost
