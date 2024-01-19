0ee58cb231f5df07974df62a8fe420c4bc65b120
Praise-Atadja <p.atadja@alustudent.com>
2024-01-19 11:48:51 +0200

    Update 21-deep_neural_network.py

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -90,17 +90,10 @@ class DeepNeuralNetwork:
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """Calculates one pass of gradient descent on the neural network"""
-        m = Y.shape[1]
+        dz = cache["A{}".format(self.__L)] - Y
         for i in range(self.__L, 0, -1):
-            A = cache['A{}'.format(i - 1)]
-            if i == self.__L:
-                dz = cache['A{}'.format(i)] - Y
-            else:
-                dz = np.multiply(
-                    da, np.multiply(A, 1 - A)
-                )
-            dw = np.matmul(dz, A.T) / m
-            db = np.sum(dz, axis=1, keepdims=True) / m
-            da = np.matmul(self.__weights['W{}'.format(i)].T, dz)
-            self.__weights['W{}'.format(i)] -= alpha * dw
-            self.__weights['b{}'.format(i)] -= alpha * db
+            db = (np.sum(dz, axis=1, keepdims=True) / Y.shape[1])
+            dw = (np.matmul(cache["A{}".format(i - 1)], dz.T) / Y.shape[1])
+            dz = np.matmul(self.__weights["W{}".format(i)].T, dz) * (cache["A{}".format(i - 1)] * (1 - cache["A{}".format(i - 1)]))
+            self.__weights["W{}".format(i)] = self.__weights["W{}".format(i)] - (alpha * dw).T
+            self.__weights["b{}".format(i)] = self.__weights["b{}".format(i)] - (alpha * db)
