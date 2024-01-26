f640cda1587ae07e4b6e8156346d728631b8cb2f
Praise-Atadja <p.atadja@alustudent.com>
2024-01-26 14:57:07 +0200

    Create 6-main.py

---
+++ supervised_learning/tensorflow/tests/6-main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Function builds, trains, and saves a neural network classifier:"""
+
+
+import numpy as np
+import tensorflow as tf
+import numpy as np
+import tensorflow as tf
+import tensorflow as tf
+train = __import__('6-train').train
+
+
+def one_hot(Y, classes):
+    """convert an array to a one-hot matrix"""
+    one_hot = np.zeros((Y.shape[0], classes))
+    one_hot[np.arange(Y.shape[0]), Y] = 1
+    return one_hot
+
+if __name__ == '__main__':
+    lib= np.load('../data/MNIST.npz')
+    X_train_3D, Y_train = lib['X_train'], lib['Y_train']
+    X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
+    Y_train_oh = one_hot(Y_train, 10)
+    X_valid_3D, Y_valid = lib['X_valid'], lib['Y_valid']
+    X_valid = X_valid_3D.reshape((X_valid_3D.shape[0], -1))
+    Y_valid_oh = one_hot(Y_valid, 10)
+
+    layer_sizes = [256, 256, 10]
+    activations = [tf.nn.tanh, tf.nn.tanh, None]
+    alpha = 0.01
+    iterations = 1000
+
+    tf.set_random_seed(0)
+    model = train(X_train, Y_train_oh, X_valid, Y_valid_oh, layer_sizes,
+            activations, alpha, iterations, save_path="./model.ckpt")
+    save_path = model.save()
+    print("Model saved in path: {}".format(save_path))
