f60704f2167d928e38df547293426f21a058ebd1
Praise-Atadja <p.atadja@alustudent.com>
2024-01-26 13:59:15 +0200

    Forward Propagation

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -14,7 +14,7 @@ layer_sizes is a list containing the number of nodes in each layer of the networ
 activations is a list containing the activation functions for each layer of the network
 
 Returns: the prediction of the network in tensor form
-For this function, you should import your create_layer function with create_layer = __import__('1-create_layer').create_layer
+
     """
     for i in range(len(layer_sizes)):
         if i is 0:
