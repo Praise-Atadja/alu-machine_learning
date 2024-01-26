ed0f5704e00921a4907bbd5bf301572372f00424
Praise-Atadja <p.atadja@alustudent.com>
2024-01-26 15:36:05 +0200

    builds, trains, and saves

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """
-Defines a function that builds, trains, and saves
+Function that builds, trains, and saves
 neural network classifier
 """
 
 
 import tensorflow as tf
-
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
