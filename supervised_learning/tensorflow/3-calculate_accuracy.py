2966503939ceaf44758021fea54ea409181a27c4
Praise-Atadja <p.atadja@alustudent.com>
2024-01-26 14:10:17 +0200

    Accuracy

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -14,7 +14,7 @@ def calculate_accuracy(y, y_pred):
     Returns: a tensor containing the decimal accuracy of the prediction
     hint: accuracy = correct_predictions / all_predictions
     """
-    T= tf.argmax(y, axis=1)
+    T = tf.argmax(y, axis=1)
     correct_predictions = tf.equal(tf.argmax(y_pred, axis=1), T)
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
     return accuracy
