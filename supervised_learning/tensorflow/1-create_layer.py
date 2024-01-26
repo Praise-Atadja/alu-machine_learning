d8d69c9843734d29ff7d020d53acbca50493e72b
Praise-Atadja <p.atadja@alustudent.com>
2024-01-26 13:36:48 +0200

    Layers

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -11,7 +11,7 @@ def create_layer(prev, n, activation):
 prev is the tensor output of the previous layer
 n is the number of nodes in the layer to create
 activation is the activation function that the layer should use
-use tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG") 
+use tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 to implement He et. al initialization for the layer weights
 each layer should be given the name layer
 
@@ -19,8 +19,5 @@ Returns: the tensor output of the layer
     """
     weights_initializer = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
-    layer = tf.layers.Dense(n,
-        activation=activation,
-        name="layer",
-        kernel_initializer=weights_initializer)
+    layer = tf.layers.Dense(n, activation=activation, name="layer", kernel_initializer=weights_initializer)
     return (layer(prev))
