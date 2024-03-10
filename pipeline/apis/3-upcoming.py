44d19b39a7711cc87dae1011cac8a13fb80f6aa9
Praise-Atadja <p.atadja@alustudent.com>
2024-03-10 19:55:49 +0200

    Update 3-upcoming.py

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python3
-""" Displays information about the upcoming SpaceX launch. """
+"""script that displays the upcoming launch with these information"""
+import sys
+import requests as rq
+import time
 
 
 if __name__ == '__main__':
-    import requests
-
-    base_URL = 'https://api.spacexdata.com/'
-
-    response = requests.get(base_URL + 'v5/launches/latest')
-
-    if response.ok:
-        latest_launch = response.json()
-
-        launch_name, date, rocket_ID, launchpad_ID = (
-            latest_launch['name'],
-            latest_launch['date_local'],
-            latest_launch['rocket'],
-            latest_launch['launchpad'],
-        )
-
-        rocket_name = requests.get(
-            base_URL + 'v4/rockets/' + rocket_ID).json()['name']
-
-        launchpad = requests.get(
-            base_URL + 'v4/launchpads/' + launchpad_ID).json()
-
-        launchpad_name, launchpad_locality = (
-            launchpad['name'],
-            launchpad['locality']
-        )
-
-        print('{} ({}) {} - {} ({})'.format(
-            launch_name,
-            date,
-            rocket_name,
-            launchpad_name,
-            launchpad_locality
-        ))
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    r = rq.get(url)
+    launches = sorted(r.json(), key=lambda i: i['date_unix'])
+    date_unix = launches[0]['date_unix']
+    for i in r.json():
+        if i['date_unix'] == date_unix:
+            launch_name = i['name']
+            date = i['date_local']
+            rocket_id = i['rocket']
+            launchpad_id = i['launchpad']
+            break
+    url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
+    r = rq.get(url)
+    rocket_name = r.json()['name']
+    url = "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id)
+    r = rq.get(url)
+    lpad_name = r.json()['name']
+    lpad_locality = r.json()['locality']
+    print("{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
+                                        lpad_name, lpad_locality))
