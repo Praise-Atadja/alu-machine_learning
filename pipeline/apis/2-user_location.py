d263852650fa04cf8a56bba776cf447e66bdd8ea
Praise-Atadja <p.atadja@alustudent.com>
2024-03-10 22:03:26 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
-""" Script for getting user location"""
-import sys
-import requests
-import time
+""" Prints the location of a specific GitHub user. """
 
+import datetime
+import requests
+import sys
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
-    res = response.json()
 
-    if response.status_code == 200:
-        print(res['location'])
-    elif response.status_code == 404:
+
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code == 404:
         print('Not found')
     elif response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
+        minutes_until_reset = int(
+            (
+                datetime.fromtimestamp(
+                    int(response.headers['X-RateLimit-Reset']))
+                - datetime.now()
+            ).total_seconds() / 60
+        )
+        print('Reset in {} min'.format(minutes_until_reset))
+    elif response.status_code == 200:
+        print(response.json()['location'])
