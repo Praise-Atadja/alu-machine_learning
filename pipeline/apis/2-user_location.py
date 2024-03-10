4e90824b28cc558ec3585ada21e0e8afe9c6a14e
Praise-Atadja <p.atadja@alustudent.com>
2024-03-10 22:32:48 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -4,18 +4,19 @@
 import time
 import requests
 import sys
-
-if __name__ == '__main__':
+def print_location():
+    """print location of user"""
     url = sys.argv[1]
     response = requests.get(url)
-    res = response.json()
+    data = response.json()
 
     if response.status_code == 200:
-        print(res['location'])
-    elif response.status_code == 404:
-        print('Not found')
+        if 'location' in data and data['location']:
+            print(data['location'])
+        else:
+            print("Not found")
     elif response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
+        print("Reset in {} min".format(
+            (int(response.headers['X-Ratelimit-Reset']) -
+            int(response.headers['X-Ratelimit-Reset']))
+        ))
