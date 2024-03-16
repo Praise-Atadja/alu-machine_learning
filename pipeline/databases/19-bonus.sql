d4001223b8e6c7a7d774d349733bb62f0f18314d
Praise-Atadja <p.atadja@alustudent.com>
2024-03-16 17:21:36 +0200

    creates a stored procedure AddBonus

---
+++ pipeline/databases/19-bonus.sql
@@ -0,0 +1,15 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
+CREATE PROCEDURE AddBonus(
+	IN user_id_new INTEGER,
+	IN project_name varchar(255),
+	IN score_new INTEGER)
+	BEGIN
+		IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
+			INSERT INTO projects(name) VALUES (project_name);
+		END IF;
+		INSERT INTO corrections(user_id, project_id, score)
+			VALUES(user_id_new,
+		(SELECT id FROM projects WHERE name = project_name),
+			score_new);
+	END //
+delimiter;
