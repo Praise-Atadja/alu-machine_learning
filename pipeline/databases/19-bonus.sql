8339a553b540464c83d769a84e1987dbcf814ab4
Praise-Atadja <p.atadja@alustudent.com>
2024-03-16 17:24:51 +0200

    Update 19-bonus.sql

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -1,15 +1,14 @@
--- creates a stored procedure AddBonus that adds a new correction for a student.
-CREATE PROCEDURE AddBonus(
-	IN user_id_new INTEGER,
-	IN project_name varchar(255),
-	IN score_new INTEGER)
-	BEGIN
-		IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
-			INSERT INTO projects(name) VALUES (project_name);
-		END IF;
-		INSERT INTO corrections(user_id, project_id, score)
-			VALUES(user_id_new,
-		(SELECT id FROM projects WHERE name = project_name),
-			score_new);
-	END //
-delimiter;
+-- creates a procedure
+DELIMITER $$
+CREATE PROCEDURE AddBonus (
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT)
+BEGIN
+    IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name) THEN
+        INSERT INTO projects(name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES(user_id, (SELECT id FROM projects WHERE name=project_name), score);
+END $$
+DELIMITER ;
