b1b060b16d5b088e0eece7b45b0e54ee1f405263
Praise-Atadja <p.atadja@alustudent.com>
2024-03-19 01:46:57 +0200

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
-""" Module used to
+"""
+Script that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    [summary]
-    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
 
-    method_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    db_address = 'mongodb://127.0.0.1:27017'
-
-    client = MongoClient(db_address)
-
-    # create dict of all dbs and its collections
-    d = dict((db, [collection for collection in client[db].collection_names()])
-            for db in client.database_names())
-
-    # validate logs and nginx exists
-    # if ('logs' not in d.keys()):
-    #    exit()
-
-    collection = client.logs.nginx  # client.database.collection
-    docs = collection.count_documents({})
-    print("{} logs".format(docs))
+    num_doc = logs_collection.count_documents({})
+    print("{} logs".format(num_doc))
 
     print("Methods:")
-    for method in method_list:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    d = {"method": "GET", "path": "/status"}
-    GET_status = collection.count_documents(d)
-    print("{} status check".format(GET_status))
+    for method in methods:
+        num_method = logs_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, num_method))
+
+    filter_path = {"method": "GET", "path": "/status"}
+    num_path = logs_collection.count_documents(filter_path)
+    print("{} status check".format(num_path))
