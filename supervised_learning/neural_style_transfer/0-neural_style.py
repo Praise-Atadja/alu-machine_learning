5a5454669ccc8b86233f700188b752d0e94e3e56
Praise-Atadja <p.atadja@alustudent.com>
2024-05-19 22:20:51 +0200

    fix errors

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,74 +1,61 @@
 #!/usr/bin/env python3
+"""Neural style transfer"""
 
-"""useless comments"""
 
 import numpy as np
 import tensorflow as tf
 
 
-def check_image_channel_input(img, source):
-    """
-    Check the channel of given image
-    :param img: The image
-    :param source: The variable name to error message
-    :return:
-    """
-    if type(img) != np.ndarray or img.shape[-1] != 3:
-        raise TypeError(
-            "{} must be a numpy.ndarray with shape (h, w, 3)".format(source)
-        )
+class NST():
+    """Class the performs neural style transfer"""
 
-
-def check_hyperparameter_input(hyperparameter, source):
-    """
-    Check given hyperparameter
-    :param hyperparameter: The hyperparameter
-    :param source: The variable name to error message
-    :return:
-    """
-    if type(hyperparameter) not in [float, int] or hyperparameter < 0:
-        raise TypeError("{} must be a non-negative number".format(source))
-
-
-class NST:
-    """Neural style transfer model"""
-
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                    'block4_conv1', 'block5_conv1']
+    style_layers = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1',
+        'block4_conv1',
+        'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """
-        Init function class
-        :param style_image: The style_image (?, ?, 3)
-        :param content_image: The content image (?, ?, 3)
-        :param alpha: The alpha parameter
-        :param beta: The beta parameter
-        """
+        """Class initializer"""
+        if not isinstance(
+                style_image, np.ndarray) or style_image.shape[-1] != 3:
+            raise TypeError(
+                'style_image must be a numpy.ndarray with shape (h, w, 3)')
+        if isinstance(
+                content_image, np.ndarray) or content_image.shape[-1] != 3:
+            raise TypeError(
+                'content_image must be a numpy.ndarray with shape (h, w, 3)')
+        if alpha < 0:
+            raise TypeError('alpha must be a non-negative number')
+        if beta < 0:
+            raise TypeError('beta must be a non-negative number')
         tf.enable_eager_execution()
-        check_image_channel_input(style_image, "style_image")
-        check_image_channel_input(content_image, "content_image")
+
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
-        check_hyperparameter_input(alpha, "alpha")
-        check_hyperparameter_input(beta, "beta")
         self.alpha = alpha
         self.beta = beta
 
     @staticmethod
     def scale_image(image):
-        """
-        Scale the image to (1, 512 or less, 512 or less, 3)
-        :param image: The given image to resize
-        :return: The resized image
-        """
-        check_image_channel_input(image, "image")
+        """Rescales image appropriately"""
+        tf.enable_eager_execution()
 
-        max_dim = max(image.shape[:-1])
-        ratio_dims = 512 / max_dim
+        if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
+            raise TypeError(
+                'image must be a numpy.ndarray with shape (h, w, 3)')
 
-        new_dims = tuple([int(dim * ratio_dims) for dim in image.shape[:-1]])
-        image = tf.expand_dims(image, 0)  # [1, h, w, 3]
-        resized_image = tf.image.resize_bicubic(image, new_dims) / 255
+        height, width, _ = image.shape
 
-        return tf.clip_by_value(resized_image, 0.0, 1.0)
+        if height > width:
+            new_h = 512
+            new_w = (new_h * width) / height
+        else:
+            new_w = 512
+            new_h = (new_w * height) / width
+
+        resized = tf.image.resize_images(
+            image, (new_h, new_w), method=tf.image.ResizeMethod.BICUBIC)
+        return resized
