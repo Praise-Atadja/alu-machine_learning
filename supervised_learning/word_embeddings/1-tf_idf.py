037dcdc92ab2f8e147c53948cd4241c2a5e103fc
Praise-Atadja <p.atadja@alustudent.com>
2024-06-02 18:00:24 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -8,7 +8,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def tf_idf(sentences, vocab=None):
     """function that creates a TF-IDF embedding"""
 
-
     vectorizer = TfidfVectorizer(vocabulary=vocab)
     X = vectorizer.fit_transform(sentences)
 
