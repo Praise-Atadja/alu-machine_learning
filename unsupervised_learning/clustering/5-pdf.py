e1336783970f9521d12546e865f470dabfad6bcc
Praise-Atadja <p.atadja@alustudent.com>
2024-06-30 21:20:42 +0200

    Update 5-pdf.py

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python3
-"""PDF module"""
+
+
+"""useless comment"""
 
 
 import numpy as np
 
 
 def pdf(X, m, S):
-    """Calculate probability density function of Gaussian distribution"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """
+    Compute the pdf for a Gaussian Mixture Model
+    :param X: The dataset
+    :param m: The means
+    :param S: Teh covarance matrix
+    :return: The pdf
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
-    if not isinstance(m, np.ndarray) or m.ndim != 1:
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
         return None
-    if not isinstance(S, np.ndarray) or S.ndim != 2:
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
         return None
-    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+    if m.shape[0] != X.shape[1]:
+        return None
+    if S.shape[0] != X.shape[1]:
         return None
     if S.shape[0] != S.shape[1]:
         return None
 
-    d = X.shape[0]
+    n, d = X.shape
 
-    A = 1.0 / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(S))
-    B = np.matmul(np.linalg.inv(S), (X - m).T)
-    C = np.expp(-0.5 * np.sum((X - m).T * M, axis=0))
-    P = A * C
-    P = np.maximum(P, 1e-300)
+    coef = 1 / ((2 * np.pi)**(d / 2) * np.sqrt(np.linalg.det(S)))
+    exponetial_arg = np.sum(
+        np.matmul(
+            (X - m),
+            np.linalg.inv(S)
+        ) * (X - m),
+        axis=1
+    ) / - 2
 
-    return P
+    return np.maximum(coef * np.exp(exponetial_arg), 1e-300)
