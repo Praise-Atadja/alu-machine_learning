6038c7f871f786ef3b9000942582bb296fa9fc78
Praise-Atadja <p.atadja@alustudent.com>
2024-06-30 21:25:17 +0200

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Expectation module"""
+
+
+""" calculates the expectation step in the EM algorithm for a GMM"""
 
 
 import numpy as np
@@ -7,37 +9,38 @@ pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """Calculate the expectation step in EM algo for GMM"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """
+    Compute the expectation (ie posterior probs and likelihood)
+    :param X: The dataset
+    :param pi: The prior probs
+    :param m: The means
+    :param S: The covariance matrix
+    :return: The posterior probs and the likelihood
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if not isinstance(pi, np.ndarray) or pi.ndim != 1:
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
         return None, None
-    if not isinstance(m, np.ndarray) or m.ndim != 2:
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
         return None, None
-    if not isinstance(S, np.ndarray) or S.ndim != 3:
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
         return None, None
-
-    n, d = X.shape
-
-    if pi.shape[0] > n:
+    if m.shape[1] != X.shape[1]:
         return None, None
-    i = pi.shape[0]
-    if m.shape[0] != i or m.shape[1] != d:
+    if S.shape[1] != X.shape[1] or X.shape[1] != S.shape[2]:
         return None, None
-    if S.shape[0] != i or S.shape[1] != d or S.shape[2] != d:
+    if pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]:
         return None, None
     if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
 
-    z = np.zeros((i, n))
+    num_of_clust = len(pi)
+    probas = np.zeros(shape=(num_of_clust, X.shape[0]))
+    for i in range(num_of_clust):
+        probas[i] = pdf(X, m[i], S[i]) * pi[i]
 
-    for j in range(i):
-        PDF = pdf(X, m[j], S[j])
-        z[j] = pi[j] * PDF
+    maginal = np.sum(probas, axis=0)
+    probas = probas / maginal
+    max_likelihood = np.sum(np.log(maginal))
 
-    sums = np.sum(z, axis=0, keepdims=True)
-    z /= sums
-
-    logs = np.sum(np.log(sums))
-
-    return z, logs
+    return probas, max_likelihood
