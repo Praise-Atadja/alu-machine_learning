0a48646be1a1bd675670e1df0db5b909f9d68f43
Praise-Atadja <p.atadja@alustudent.com>
2024-06-30 21:17:42 +0200

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Initialize GMM"""
+
+
+"""useless comment"""
 
 
 import numpy as np
@@ -7,16 +9,23 @@ kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """Initialize variables for a gaussian mixture model"""
+    """
+    Initilize variable (pi, means, covariance matrix)
+    for a Gaussian Mixture Model
+    :param X: The dataset
+    :param k: The number of cluster
+    :return: The prior probs, means and covariance matrix
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
+
     if not isinstance(k, int) or k <= 0:
         return None, None, None
 
-    n, d = X.shape
-    centroids, clss = kmeans(X, k, iterations=1000)
-    pi = np.ones(k) / k
-    m = centroids
+    _, d = X.shape
+
+    pi = np.ones(shape=(k,)) / k
+    m, _ = kmeans(X, k)
     S = np.tile(np.identity(d), (k, 1)).reshape((k, d, d))
 
     return pi, m, S
