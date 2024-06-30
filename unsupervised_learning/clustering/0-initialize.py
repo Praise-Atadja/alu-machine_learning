7df36f1907ad36154bfcb04e1ea2500c527ea564
Praise-Atadja <p.atadja@alustudent.com>
2024-06-30 20:35:21 +0200

    Update 0-initialize.py

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
-"""
-0-initialize.py
-"""
+"""Initialize K-means"""
+
+
 import numpy as np
 
 
 def initialize(X, k):
-    """function that initializes cluster centroids for K-means"""
-
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """Initialize cluster centroids"""
+    if not isinstance(k, int) or k <= 0:
         return None
-
-    # n: number of dada points
-    # d: dimension of each data point
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
     n, d = X.shape
-    # print(X.shape)
-    # print(X)
+    clusters = np.random.uniform(
+        np.min(X, axis=0), np.max(X, axis=0), size=(k, d))
 
-    if not isinstance(k, int) or k <= 0 or k > n:
-        return None
-
-    # Sample k centroids from a random.uniform distribution;
-    # output is an array of coordinates
-    mu = np.random.uniform(low=np.min(X, axis=0),
-                           high=np.max(X, axis=0),
-                           size=(k, d))
-    return mu
+    return clusters
