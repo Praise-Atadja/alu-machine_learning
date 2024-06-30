b901f3b868a32c4f7f25c72665ff66545c088800
Praise-Atadja <p.atadja@alustudent.com>
2024-06-30 21:27:12 +0200

    Update 7-maximization.py

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
-"""Maximization module"""
+
+
+""" calculates the maximization step in the EM algorithm for a GMM"""
 
 
 import numpy as np
 
 
 def maximization(X, g):
-    """Calculate maximization step in EM algo for GMM"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """
+    Update the prior, the means and the covariance matrix
+    :param X: The datdaset
+    :param g: The posterior probs
+    :return: The new priors, means and covariance
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(g, np.ndarray) or g.ndim != 2:
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    if g.shape[1] != X.shape[0]:
+        return None, None, None
+    cluster = np.sum(g, axis=0)
+    cluster = np.sum(cluster)
+    if int(cluster) != X.shape[0]:
         return None, None, None
 
-    n, d = X.shape
+    num_of_coords = X.shape[1]
+    num_of_clusters = g.shape[0]
 
-    if g.shape[1] != n:
-        return None, None, None
-    k = g.shape[0]
-    if g.shape[0] != k:
-        return None, None, None
+    pi = np.sum(g, axis=1) / X.shape[0]
 
-    if not np.isclose(np.sum(g, axis=0), np.ones(n,)).all():
-        return None, None, None
+    means = np.zeros(shape=(num_of_clusters, num_of_coords))
+    covs = np.zeros(shape=(num_of_clusters, num_of_coords, num_of_coords))
+    for cluster in range(num_of_clusters):
+        means[cluster] = np.matmul(g[cluster], X) / np.sum(g, axis=1)[cluster]
+        norm = X - means[cluster]
+        covs[cluster] = (np.matmul(g[cluster] * norm.T, norm) /
+                         np.sum(g, axis=1)[cluster])
 
-    return pi, m, S
+    return pi, means, covs
