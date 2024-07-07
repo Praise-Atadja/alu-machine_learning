e4538c6fc01157d659c8b4b1b07a38ecee18a76f
Praise-Atadja <p.atadja@alustudent.com>
2024-07-07 09:52:54 +0200

    Update 2-absorbing.py

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
-"""Absorbing chains"""
-
-
+"""absorbing chain"""
 import numpy as np
 
 
 def absorbing(P):
-    """Determine if markov chain is absorbing"""
-    if not isinstance(P, np.ndarray) or P.ndim != 2:
-        return False
-
-    n, n_s = P.shape
-    if n != n_s:
-        return False
-    sums = np.sum(P, axis=1)
-    for i in sums:
-        if not np.isclose(i, 1):
-            return False
-
-    diagonal = np.diag(P)
-    if (diagonal == 1).all():
+    """determines if a markov chain is absorbing"""
+    if len(P.shape) != 2:
+        return None
+    n1, n2 = P.shape
+    if (n1 != n2) or type(P) is not np.ndarray:
+        return None
+    diag = np.diagonal(P)
+    if (diag == 1).all():
         return True
-
-    absorb = (diagonal == 1)
-    for x in range(len(diagonal)):
-        for y in range(len(diagonal)):
-            if P[x, y] > 0 and absorb[y]:
-                absorb[x] = 1
-    if (absorb == 1).all():
+    if not (diag == 1).any():
+        return False
+    test = np.where(diag == 1)
+    if len(test[0]) == 1:
+        for x in range(n1):
+            new = P[:, [x]]
+            count = np.where(new > 0)[0]
+            if len(count) < 2 and x + 1 != n1:
+                return False
+            if len(count) >= 1 and x + 1 == n1:
+                return True
+    if len(test[0]) == 2:
         return True
-
     return False
