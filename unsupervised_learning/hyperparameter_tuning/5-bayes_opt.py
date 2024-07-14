4e19a51284e1e767f9dd058d309b693d077288a0
Praise-Atadja <p.atadja@alustudent.com>
2024-07-14 11:42:26 +0200

    Update 5-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -1,66 +1,53 @@
 #!/usr/bin/env python3
-"""Initialize Bayesian Optimization"""
-
-
+"""hyperparameter tuning"""
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization():
-    """Perform BO on noiseless 1D Gaussian Process"""
+    """BayesianOptimization"""
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
                  xsi=0.01, minimize=True):
-        """Class init constructor"""
+        """class constructor"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        min, max = bounds
-        self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
-        """Calculate next best sample location"""
-        pr1, pr2 = self.gp.predict(self.X_s)
-
-        if self.minimize is False:
-            tmp = np.amax(self.gp.Y)
-            res = pr1 - tmp - self.xsi
+        """calculates the next best sample location"""
+        mu, sigma = self.gp.predict(self.X_s)
+        if self.minimize is True:
+            Y_sample = np.min(self.gp.Y)
+            imp = Y_sample - mu - self.xsi
         else:
-            tmp = np.amin(self.gp.Y)
-            res = tmp - pr1 - self.xsi
-
-        zer = np.zeros(pr2.shape)
-        for i in range(len(pr2)):
-            if pr2[i] != 0:
-                zer[i] = res[i] / pr2[i]
-            else:
-                zer[i] = 0
-        EI = res * norm.cdf(zer) + pr2 * norm.pdf(zer)
-        EI[pr2 == 0.0] = 0.0
-        X_next = self.X_s[np.argmax(EI)]
-
-        return X_next, EI
+            Y_sample = np.max(self.gp.Y)
+            imp = mu - Y_sample - self.xsi
+        with np.errstate(divide='warn'):
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+        X_next = self.X_s[np.argmax(ei)]
+        return X_next, ei
 
     def optimize(self, iterations=100):
-        """Optimize the black box"""
-        pos = []
-
+        """optimizes the black-box function"""
+        all_next = []
         for i in range(iterations):
-            fx, _ = self.acquisition()
-            if fx in pos:
+            x_next, _ = self.acquisition()
+            if x_next in all_next:
                 break
-            fy = self.f(fx)
-            self.gp.update(fx, fy)
-            pos.append(fx)
-
+            y_next = self.f(x_next)
+            self.gp.update(x_next, y_next)
+            all_next.append(x_next)
         if self.minimize is True:
-            Y_opt = np.min(self.gp.Y)[np.newaxis]
-            tmp = np.argmin(self.gp.Y)
+            index = np.argmin(self.gp.Y)
         else:
-            Y_opt = np.max(self.gp.Y)[np.newaxis]
-            tmp = np.argmax(self.gp.Y)
-        X_opt = self.gp.X[tmp]
-
-        return X_opt, Y_opt
+            index = np.argmax(self.gp.Y)
+        self.gp.X = self.gp.X[:-1]
+        x_next = self.gp.X[index]
+        y_next = self.gp.Y[index]
+        return x_next, y_next
